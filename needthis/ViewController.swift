8c21fc9aa695712213a41efa8fe42b0f20060d1e
twairball <twairball@yahoo.com>
2015-08-11 15:53:39 +0800

    Initial Commit

---
+++ needthis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  needthis
+//
+//  Created by JERRY LIU on 11/8/15.
+//  Copyright (c) 2015 ONTHETALL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
